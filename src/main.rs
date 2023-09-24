ee24c8e5e3b8eb921e0dce080528d16005aeec04
bakedsnake <gilferrandm@gmail.com>
2023-09-24 13:28:58 +0200

    add bmr formulas, and calculate when using the c flag

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::io;
 use std::env;
 
+// BMR Formula
+pub fn male_bmr(w: f32, h: f32, a: f32) -> f32 {
+    return 10.0 * w + 6.25 * h - 5.0 * a + 5.0;
+}
+pub fn female_bmr(w: f32, h: f32, a: f32) -> f32 {
+    return 10.0 * w + 6.25 * h - 5.0 * a - 161.0;
+}
+
 // Convert cm to m
 pub fn to_meters(h: f32) -> f32 {
     return h / 100.0;
@@ -48,15 +56,25 @@ pub fn classification(b: f32) {
 }
 
 fn main() {
-    let bmi = do_bmi();
     let args: Vec<String> = env::args().collect();
 
     // Calculate Calorie consumption
     if args[1] == "c" {
-        println!("Your BMI score is {}", bmi);
-        println!("You chose {} days.", args[2]);
+        if args[5] == "male" {
+            let a2 = args[2].clone() + ".0";
+            let a3 = args[3].clone() + ".0";
+            let a4 = args[4].clone() + ".0";
+            let bmr = male_bmr(a3.trim().parse().unwrap(), a2.trim().parse().unwrap(), a4.trim().parse().unwrap());
+            let cals = bmr * 1.375;
+            println!("To reach your desired weight you need {} calories daily.", cals.ceil());
+        } else if args[5] == "female" {
+            let bmr = female_bmr(args[3].trim().parse().unwrap(), args[2].trim().parse().unwrap(), args[4].trim().parse().unwrap());
+            let cals = bmr * 1.375;
+            println!("To reach your desired weight you need {} calories daily.", cals.ceil());
+        }
     // Calculate BMI
     } else if args[1] == "b" {
+        let bmi = do_bmi();
         println!("Your BMI score is {}", bmi);
         classification(bmi);
     // Print help
